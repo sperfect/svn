ad992d0ce5b24db472dfbffe485ffce66edbbf25
sperfect@gmail.com <sperfect@gmail.com@386ac314-8ea1-3195-d3b8-c3d39170a2dd>
2014-10-08 20:34:15 +0000

    objectify

---
+++ sperfect-jukebox/JuqboxApi/src/gr/sperfect/juqbox/server/api/OfyService.java
@@ -0,0 +1,25 @@
+package gr.sperfect.juqbox.server.api;
+
+import com.googlecode.objectify.Objectify;
+import com.googlecode.objectify.ObjectifyFactory;
+import com.googlecode.objectify.ObjectifyService;
+
+public class OfyService {
+
+	 static {
+	        factory().register(Room.class);	   
+	        factory().register(Song.class);
+	        
+	        // ...etc
+	    }
+	 
+	 public static Objectify ofy() {
+	        return ObjectifyService.ofy();
+	    }
+
+	    public static ObjectifyFactory factory() {
+	        return ObjectifyService.factory();
+	    }
+
+
+}

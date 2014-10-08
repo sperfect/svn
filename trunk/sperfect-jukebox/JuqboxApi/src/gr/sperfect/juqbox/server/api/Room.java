272f949ff2fa808c5123b5b38f8c54a6028c6fd3
sperfect@gmail.com <sperfect@gmail.com@386ac314-8ea1-3195-d3b8-c3d39170a2dd>
2014-10-08 20:55:21 +0000

    objectify annotate

--- trunk/sperfect-jukebox/JuqboxApi/src/gr/sperfect/juqbox/server/api/Room.java
+++ trunk/sperfect-jukebox/JuqboxApi/src/gr/sperfect/juqbox/server/api/Room.java
@@ -1,5 +1,28 @@
 package gr.sperfect.juqbox.server.api;
 
+import com.googlecode.objectify.Ref;
+import com.googlecode.objectify.annotation.Entity;
+import com.googlecode.objectify.annotation.Id;
+import com.googlecode.objectify.annotation.Load;
+
+@Entity
 public class Room {
+
+	@Id
+	Long id;
+
+	private Room() {
+	};
+
+	@Load
+	private Ref<User> creator;
+
+	public User getCreator() {
+		return creator.get();
+	}
+
+	public void setCreator(User creator) {
+		this.creator = Ref.create(creator);
+	}
 
 }
--- trunk/sperfect-jukebox/JuqboxApi/src/gr/sperfect/juqbox/server/api/Song.java
+++ trunk/sperfect-jukebox/JuqboxApi/src/gr/sperfect/juqbox/server/api/Song.java
@@ -1,5 +1,12 @@
 package gr.sperfect.juqbox.server.api;
 
-public class Song {
+import com.googlecode.objectify.annotation.Entity;
+import com.googlecode.objectify.annotation.Id;
 
+@Entity
+public class Song {
+	@Id Long id;
+
+	 protected Song() {};
+	 
 }
